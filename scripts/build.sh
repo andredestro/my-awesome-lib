f8fd89d466e1a22c97740b84158dad925f7ba6cc
Andre Destro <andre.destro@outsystems.com>
2025-08-04 11:54:22 +0100

    chore: update build.sh to use PROJECT_NAME placeholder

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,24 +3,24 @@ cd ..
 rm -rf build
 
 xcodebuild archive \
--scheme LibTemplatePlaceholder \
+-scheme __PROJECT_NAME__ \
 -configuration Release \
 -destination 'generic/platform=iOS Simulator' \
--archivePath './scripts/build/LibTemplatePlaceholder.framework-iphonesimulator.xcarchive' \
+-archivePath './scripts/build/__PROJECT_NAME__.framework-iphonesimulator.xcarchive' \
 SKIP_INSTALL=NO \
 BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
 
 
 xcodebuild archive \
--scheme LibTemplatePlaceholder \
+-scheme __PROJECT_NAME__ \
 -configuration Release \
 -destination 'generic/platform=iOS' \
--archivePath './scripts/build/LibTemplatePlaceholder.framework-iphoneos.xcarchive' \
+-archivePath './scripts/build/__PROJECT_NAME__.framework-iphoneos.xcarchive' \
 SKIP_INSTALL=NO \
 BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
 
 
 xcodebuild -create-xcframework \
--framework './scripts/build/LibTemplatePlaceholder.framework-iphonesimulator.xcarchive/Products/Library/Frameworks/LibTemplatePlaceholder.framework' \
--framework './scripts/build/LibTemplatePlaceholder.framework-iphoneos.xcarchive/Products/Library/Frameworks/LibTemplatePlaceholder.framework' \
--output './scripts/build/LibTemplatePlaceholder.xcframework'
+-framework './scripts/build/__PROJECT_NAME__.framework-iphonesimulator.xcarchive/Products/Library/Frameworks/__PROJECT_NAME__.framework' \
+-framework './scripts/build/__PROJECT_NAME__.framework-iphoneos.xcarchive/Products/Library/Frameworks/__PROJECT_NAME__.framework' \
+-output './scripts/build/__PROJECT_NAME__.xcframework'
