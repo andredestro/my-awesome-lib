6d093724a575ac0db15dbb8df788b1e186333cf3
Andre Destro <andre.destro@outsystems.com>
2025-08-04 11:29:13 +0100

    feat: add new Xcode project template for iOS library development

---
+++ __PROJECT_NAME__/__PROJECT_NAME__.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class __PROJECT_NAME__ {
+    // This class is a placeholder for the project name
+    // Add your project-specific code here
+    
+    func hello() -> String {
+        return "Hello, __PROJECT_NAME__!"
+    }
+}
