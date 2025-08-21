3c41301bd6db0b573947d06051e33523751b444b
Andre Destro <andre.destro@outsystems.com>
2025-08-21 14:17:28 +0100

    feat: new test

--- MyAwesomeLibTests/MyAwesomeLibTests.swift
+++ MyAwesomeLibTests/MyAwesomeLibTests.swift
@@ -9,4 +9,11 @@ final class MyAwesomeLibTests: XCTestCase {
         let instance = MyAwesomeLib()
         XCTAssertEqual(instance.hello(), "Hello, MyAwesomeLib!")
     }
+
+    func testExample2() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let instance = MyAwesomeLib()
+        XCTAssertEqual(instance.hello(), "Hello, MyAwesomeLib!")
+    }
 }
